3d16707a3816ea3eafbda142134acdebebacddbf
Andrew Hulterstrom <andrewhulterstrom@gmail.com>
2025-09-01 05:13:51 -0600

    remove the loader because it is causing issues in prod

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
                 <SceneManager />
               </Suspense>
             </Canvas>
-            <Loader />
+            {/* <Loader /> */}
             <Leva hidden={true} collapsed flat />
           </MenuProvider>
         </SceneProvider>
